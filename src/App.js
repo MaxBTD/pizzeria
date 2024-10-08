8cabcd7358b1a752ffe9d58b0bfdbafc214aa935
unknown <konradek982@gmail.com>
2024-10-08 11:29:53 +0200

    pizza app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 
 import './App.css';
 import PizzaMenu from './pizzaMenu';
-import pol from './img/pol.png';
 
-const pizzas = [{nr: 0, name: "Pizza pół na pół", img: {pol}, desc: "Wybierz 2 połówki pizzy w jednej i rozkoszuj się podwójnym smakiem. Cena pojawi się po skompletowaniu zamówienia. Wybierz rozmiar, kliknij w cenę i do dzieła!"},
-                {nr: 1, name: "Margherita", img: "margherita.webp", desc: "ciasto, sos pomidorowy, ser, oregano"}
+const pizzas = [{nr: 0, name: "Pizza pół na pół", desc: "Wybierz 2 połówki pizzy w jednej i rozkoszuj się podwójnym smakiem. Cena pojawi się po skompletowaniu zamówienia. Wybierz rozmiar, kliknij w cenę i do dzieła!"},
+                {nr: 1, name: "Margherita", desc: "ciasto, sos pomidorowy, ser, oregano"},
+                {nr: 2, name: "Capriciosa", desc: "ciasto, sos pomidorowy, ser, szynka, pieczarki, oregano"},
+                {nr: 3, name: "Parma", desc: "ciasto, sos pomidorowy, ser mozzarella, szynka dojrzewająca, bazylia świeża, oregano"},
+                {nr: 4, name: "Campione", desc: "ciasto, sos pomidorowy, ser, szynka, kabanosy, boczek wędzony, salami, oregano"},
+                {nr: 5, name: "Decoro", desc: "ciasto, sos pomidorowy, ser, szynka, pieczarki, papryka konserwowa, czosnek, oregano"}
 ];
 
 function App() {
   return (
-    <div className="App">
-      <PizzaMenu pizza={pizzas[0]}/>
-      <PizzaMenu pizza={pizzas[1]}/>
+    <div id="App">
+        <div id="header">
+          <h1>MAMA MIA PIZZA PIE</h1>
+        </div>
+
+        <div id="pizzas">
+          <PizzaMenu pizza={pizzas[0]}/>
+          <PizzaMenu pizza={pizzas[1]}/>
+        </div>
+        <div>
+          <PizzaMenu pizza={pizzas[2]}/>
+          <PizzaMenu pizza={pizzas[3]}/>
+        </div>
+        <div>
+          <PizzaMenu pizza={pizzas[4]}/>
+          <PizzaMenu pizza={pizzas[5]}/>
+        </div>
     </div>
   );
 }
--- src/pizzaMenu.js
+++ src/pizzaMenu.js
@@ -1,13 +1,38 @@
 import './pizzaMenu.css';
 import pol from './img/pol.png';
+import marg from './img/margherita.webp';
+import capr from './img/capriciosa.webp';
+import parm from './img/parma.webp';
+import camp from './img/campione.webp';
+import decr from './img/decoro.webp';
 
 
 function PizzaMenu(props){
+    const imagePick = () =>{
+        switch(props.pizza.nr){
+            case 0:
+                return(<img src={pol}/>);
+            case 1:
+                return(<img src={marg}/>);
+            case 2:
+                return(<img src={capr}/>);
+            case 3:
+                return(<img src={parm}/>);
+            case 4:
+                return(<img src={camp}/>);
+            case 5:
+                return(<img src={decr}/>);
+            default:
+                return(<img src={pol}/>);
+        }
+    }
     return(
-        <div classname="pizzaCell">
-            <img src={props.pizza.img}/>
-            <div>
-                <h3>{props.pizza.name}</h3>
+        <div id="pizzaCell">
+            <div id="pizzaImg">
+                {imagePick()}
+            </div>
+            <div id="pizzaDesc">
+                <h3>{(props.pizza.nr > 0) ? props.pizza.nr+".": ""} {props.pizza.name}</h3>
                 <p>{props.pizza.desc}</p>
             </div>
         </div>    
