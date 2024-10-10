1c8a0ce6ff2e85cf08fb01c764dd5be050152ef0
unknown <konradek982@gmail.com>
2024-10-10 11:30:31 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import './App.css';
 import PizzaMenu from './pizzaMenu';
 
-const pizzas = [{nr: 0, name: "Pizza pół na pół", desc: "Wybierz 2 połówki pizzy w jednej i rozkoszuj się podwójnym smakiem. Cena pojawi się po skompletowaniu zamówienia. Wybierz rozmiar, kliknij w cenę i do dzieła!"},
-                {nr: 1, name: "Margherita", desc: "ciasto, sos pomidorowy, ser, oregano"},
-                {nr: 2, name: "Capriciosa", desc: "ciasto, sos pomidorowy, ser, szynka, pieczarki, oregano"},
-                {nr: 3, name: "Parma", desc: "ciasto, sos pomidorowy, ser mozzarella, szynka dojrzewająca, bazylia świeża, oregano"},
-                {nr: 4, name: "Campione", desc: "ciasto, sos pomidorowy, ser, szynka, kabanosy, boczek wędzony, salami, oregano"},
-                {nr: 5, name: "Decoro", desc: "ciasto, sos pomidorowy, ser, szynka, pieczarki, papryka konserwowa, czosnek, oregano"}
+const pizzas = [{nr: 0, name: "Pizza pół na pół",price: 25, desc: "Wybierz 2 połówki pizzy w jednej i rozkoszuj się podwójnym smakiem. Cena pojawi się po skompletowaniu zamówienia. Wybierz rozmiar, kliknij w cenę i do dzieła!"},
+                {nr: 1, name: "Margherita",price: 28, desc: "ciasto, sos pomidorowy, ser, oregano"},
+                {nr: 2, name: "Capriciosa",price: 27, desc: "ciasto, sos pomidorowy, ser, szynka, pieczarki, oregano"},
+                {nr: 3, name: "Parma",price: 26, desc: "ciasto, sos pomidorowy, ser mozzarella, szynka dojrzewająca, bazylia świeża, oregano"},
+                {nr: 4, name: "Campione",price: 29, desc: "ciasto, sos pomidorowy, ser, szynka, kabanosy, boczek wędzony, salami, oregano"},
+                {nr: 5, name: "Decoro",price: 30, desc: "ciasto, sos pomidorowy, ser, szynka, pieczarki, papryka konserwowa, czosnek, oregano"}
 ];
 
 function App() {
@@ -16,18 +16,22 @@ function App() {
         <div id="header">
           <h1>MAMA MIA PIZZA PIE</h1>
         </div>
-
-        <div id="pizzas">
-          <PizzaMenu pizza={pizzas[0]}/>
-          <PizzaMenu pizza={pizzas[1]}/>
-        </div>
-        <div>
-          <PizzaMenu pizza={pizzas[2]}/>
-          <PizzaMenu pizza={pizzas[3]}/>
-        </div>
-        <div>
-          <PizzaMenu pizza={pizzas[4]}/>
-          <PizzaMenu pizza={pizzas[5]}/>
+        <div id="pizzasList">
+          <div></div>
+          <h1>PIZZA</h1>
+          <hr />
+          <div className="pizzas">
+            <PizzaMenu pizza={pizzas[0]}/>
+            <PizzaMenu pizza={pizzas[1]}/>
+          </div>
+          <div className="pizzas">
+            <PizzaMenu pizza={pizzas[2]}/>
+            <PizzaMenu pizza={pizzas[3]}/>
+          </div>
+          <div className="pizzas">
+            <PizzaMenu pizza={pizzas[4]}/>
+            <PizzaMenu pizza={pizzas[5]}/>
+          </div>
         </div>
     </div>
   );
--- src/pizzaMenu.js
+++ src/pizzaMenu.js
@@ -5,9 +5,12 @@ import capr from './img/capriciosa.webp';
 import parm from './img/parma.webp';
 import camp from './img/campione.webp';
 import decr from './img/decoro.webp';
+import {useState} from 'react';
+
 
 
 function PizzaMenu(props){
+    const [finalPrice, setFinalPrice] = useState(0);
     const imagePick = () =>{
         switch(props.pizza.nr){
             case 0:
@@ -34,6 +37,7 @@ function PizzaMenu(props){
             <div id="pizzaDesc">
                 <h3>{(props.pizza.nr > 0) ? props.pizza.nr+".": ""} {props.pizza.name}</h3>
                 <p>{props.pizza.desc}</p>
+                <div id="buyButton"><button onclick ={ () => {setFinalPrice({finalPrice}+props.pizza.price)}}/><p>+{props.pizza.price}.00 zł {finalPrice}</p></div>
             </div>
         </div>    
     );
